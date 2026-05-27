2ab40b8608c9cf75b78823d7dd118297f94c41d7
unknown <devalvarenga@gmail.com>
2026-05-26 21:31:09 -0300

    fix: search books response

--- script.js
+++ script.js
@@ -1,191 +1,191 @@
-const apiUrl = "https://api-books-bh0c.onrender.com/books";
+    const apiUrl = "https://api-books-bh0c.onrender.com/books";
 
 
-/* =========================
-   TROCAR PÁGINAS
-========================= */
+    /* =========================
+    TROCAR PÁGINAS
+    ========================= */
 
-function mostrarPagina(id) {
+    function mostrarPagina(id) {
 
-    const paginas =
-        document.querySelectorAll(".pagina");
+        const paginas =
+            document.querySelectorAll(".pagina");
 
-    paginas.forEach(pagina => {
+        paginas.forEach(pagina => {
 
-        pagina.style.display = "none";
-    });
-
-    document.getElementById(id)
-        .style.display = "block";
-}
-
-
-
-/* =========================
-   LISTAR LIVROS
-========================= */
-
-async function carregarLivros() {
-
-    mostrarPagina("paginaBiblioteca");
-
-    try {
-
-        const resposta = await fetch(apiUrl, {
-
-            method: "GET",
-
-            headers: {
-                "x-api-key": "123456"
-            }
+            pagina.style.display = "none";
         });
 
-        const dados =
-            await resposta.json();
-
-        const livros =
-            dados.content;
-
-        const lista =
-            document.getElementById("listaLivros");
-
-        lista.innerHTML = "";
-
-
-        if (!Array.isArray(livros)
-            || livros.length === 0) {
-
-            lista.innerHTML =
-                "<p>Nenhum livro encontrado</p>";
-
-            return;
-        }
-
-
-        livros.forEach(livro => {
-
-            lista.innerHTML += `
-
-                <div class="livro-card">
-
-                    <h3>${livro.title}</h3>
-
-                    <p>
-                        <strong>ID:</strong>
-                        ${livro.id}
-                    </p>
-
-                    <p>
-                        <strong>Autor:</strong>
-                        ${livro.author?.name || "Não informado"}
-                    </p>
-
-                    <p>
-                        <strong>Categoria:</strong>
-                        ${livro.category?.name || "Não informado"}
-                    </p>
-
-                    <p>
-                        <strong>Editora:</strong>
-                        ${livro.publisher?.name || "Não informado"}
-                    </p>
-
-                </div>
-            `;
-        });
-
-    } catch (erro) {
-
-        console.log(erro);
-
-        alert("Erro ao carregar livros");
-    }
-}
-
-
-
-/* =========================
-   CRIAR LIVRO
-========================= */
-
-async function criarLivro() {
-
-    const titulo =
-        document.getElementById("titulo").value;
-
-
-    if (titulo.trim() === "") {
-
-        alert("Digite um título");
-
-        return;
+        document.getElementById(id)
+            .style.display = "block";
     }
 
 
-    try {
 
-        const resposta = await fetch(apiUrl, {
+    /* =========================
+    LISTAR LIVROS
+    ========================= */
 
-            method: "POST",
+    async function carregarLivros() {
 
-            headers: {
+        mostrarPagina("paginaBiblioteca");
 
-                "Content-Type": "application/json",
+        try {
 
-                "x-api-key": "123456",
+            const resposta = await fetch(apiUrl, {
 
-                "Idempotency-Key":
-                    crypto.randomUUID()
-            },
+                method: "GET",
 
-            body: JSON.stringify({
-
-                title: titulo,
-
-                author: {
-                    id: 1
-                },
-
-                category: {
-                    id: 1
-                },
-
-                publisher: {
-                    id: 1
+                headers: {
+                    "x-api-key": "123456"
                 }
-            })
-        });
+            });
+
+            const dados =
+                await resposta.json();
+
+            const livros =
+                dados.content;
+
+            const lista =
+                document.getElementById("listaLivros");
+
+            lista.innerHTML = "";
 
 
-        if (!resposta.ok) {
+            if (!Array.isArray(livros)
+                || livros.length === 0) {
 
-            alert("Erro API");
+                lista.innerHTML =
+                    "<p>Nenhum livro encontrado</p>";
+
+                return;
+            }
+
+
+            livros.forEach(livro => {
+
+                lista.innerHTML += `
+
+                    <div class="livro-card">
+
+                        <h3>${livro.title}</h3>
+
+                        <p>
+                            <strong>ID:</strong>
+                            ${livro.id}
+                        </p>
+
+                        <p>
+                            <strong>Autor:</strong>
+                            ${livro.author?.name || "Não informado"}
+                        </p>
+
+                        <p>
+                            <strong>Categoria:</strong>
+                            ${livro.category?.name || "Não informado"}
+                        </p>
+
+                        <p>
+                            <strong>Editora:</strong>
+                            ${livro.publisher?.name || "Não informado"}
+                        </p>
+
+                    </div>
+                `;
+            });
+
+        } catch (erro) {
+
+            console.log(erro);
+
+            alert("Erro ao carregar livros");
+        }
+    }
+
+
+
+    /* =========================
+    CRIAR LIVRO
+    ========================= */
+
+    async function criarLivro() {
+
+        const titulo =
+            document.getElementById("titulo").value;
+
+
+        if (titulo.trim() === "") {
+
+            alert("Digite um título");
 
             return;
         }
 
 
-        alert("Livro criado!");
+        try {
 
-        document.getElementById("titulo").value = "";
+            const resposta = await fetch(apiUrl, {
 
-        carregarLivros();
+                method: "POST",
 
-    } catch (erro) {
+                headers: {
 
-        console.log(erro);
+                    "Content-Type": "application/json",
 
-        alert("Erro de conexão");
+                    "x-api-key": "123456",
+
+                    "Idempotency-Key":
+                        crypto.randomUUID()
+                },
+
+                body: JSON.stringify({
+
+                    title: titulo,
+
+                    author: {
+                        id: 1
+                    },
+
+                    category: {
+                        id: 1
+                    },
+
+                    publisher: {
+                        id: 1
+                    }
+                })
+            });
+
+
+            if (!resposta.ok) {
+
+                alert("Erro API");
+
+                return;
+            }
+
+
+            alert("Livro criado!");
+
+            document.getElementById("titulo").value = "";
+
+            carregarLivros();
+
+        } catch (erro) {
+
+            console.log(erro);
+
+            alert("Erro de conexão");
+        }
     }
-}
 
 
 
-/* =========================
-   BUSCAR LIVRO
-========================= */
+    /* =========================
+    BUSCAR LIVRO
+    ========================= */
 
-async function buscarLivro() {
+    async function buscarLivro() {
 
     const texto =
         document.getElementById("pesquisa").value;
@@ -207,9 +207,11 @@ async function buscarLivro() {
             }
         );
 
+        const dados =
+            await resposta.json();
 
         const livros =
-            await resposta.json();
+            dados.content;
 
         const lista =
             document.getElementById("listaLivros");
@@ -269,11 +271,11 @@ async function buscarLivro() {
 
 
 
-/* =========================
-   INICIALIZAÇÃO
-========================= */
+    /* =========================
+    INICIALIZAÇÃO
+    ========================= */
 
-window.onload = () => {
+    window.onload = () => {
 
-    carregarLivros();
-};
+        carregarLivros();
+    };
